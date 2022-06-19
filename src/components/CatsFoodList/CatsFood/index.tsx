d59376577fa137c2df24073668622943e45bf9df
XomekX <z_v_m_x@mail.ru>
2022-06-19 21:29:30 +0700

    fix/ removed the square on the corner

--- src/components/CatsFoodList/CatsFood/index.tsx
+++ src/components/CatsFoodList/CatsFood/index.tsx
@@ -17,10 +17,12 @@ const CatsFood: FC<ICatsFoodProps> = ({ className, catsFood, ...props }) => {
   }, []);
 
   const active = selected ? styles.active : "";
+  const activeBorder = selected ? styles.cardBorderActive : "";
   const disabledCard = disabled ? styles.disabledCard : "";
 
   const catsFoodStyles = filterStyles([styles.catsFood, className]);
   const cardStyles = filterStyles([active, disabledCard, styles.card]);
+  const cardBorderStyles = filterStyles([activeBorder, styles.cardBorder]);
 
   const onClickCard = () => {
     if (!disabled) {
@@ -30,49 +32,51 @@ const CatsFood: FC<ICatsFoodProps> = ({ className, catsFood, ...props }) => {
 
   return (
     <div className={catsFoodStyles} {...props}>
-      <div
-        className={cardStyles}
-        onClick={onClickCard}
-        onMouseEnter={selected ? () => setHover(true) : () => {}}
-        onMouseLeave={() => setHover(false)}
-      >
-        {disabled && <div className={styles.maskDisabled}></div>}
-        <div className={styles.tagname}>
-          {hover && selected && !disabled ? (
-            <span className={styles.hover}>Котэ не одобряет?</span>
-          ) : (
-            "Сказачное заморское яство"
-          )}
-        </div>
-        <div className={styles.title}>
-          {catsFood.title}
-          <span>{catsFood.taste}</span>
-        </div>
-        <div>
-          <div>
-            <span className={styles.servings}>{catsFood.servings}</span>порций
-          </div>
-          <span className={styles.present}>
-            {catsFood.present === 1 ? (
-              "мышь в подарок"
-            ) : catsFood.present === 2 ? (
-              <div>
-                <span className={styles.mouseCount}>{catsFood.present}</span>
-                мыши в подарок
-              </div>
+      <div className={cardBorderStyles}>
+        <div
+          className={cardStyles}
+          onClick={onClickCard}
+          onMouseEnter={selected ? () => setHover(true) : () => {}}
+          onMouseLeave={() => setHover(false)}
+        >
+          {disabled && <div className={styles.maskDisabled}></div>}
+          <div className={styles.tagname}>
+            {hover && selected && !disabled ? (
+              <span className={styles.hover}>Котэ не одобряет?</span>
             ) : (
-              <div>
-                <span className={styles.mouseCount}>{catsFood.present}</span>
-                мышей в подарок
-                <div>заказчик доволен</div>
-              </div>
+              "Сказачное заморское яство"
             )}
-          </span>
-        </div>
-        <img className={styles.img} src={catsFood.imgSrc} alt="img" />
-        <div className={styles.circle}>
-          <div className={styles.weight}>{catsFood.weight}</div>
-          <span className={styles.kg}>кг</span>
+          </div>
+          <div className={styles.title}>
+            {catsFood.title}
+            <span>{catsFood.taste}</span>
+          </div>
+          <div>
+            <div>
+              <span className={styles.servings}>{catsFood.servings}</span>порций
+            </div>
+            <span className={styles.present}>
+              {catsFood.present === 1 ? (
+                "мышь в подарок"
+              ) : catsFood.present === 2 ? (
+                <div>
+                  <span className={styles.mouseCount}>{catsFood.present}</span>
+                  мыши в подарок
+                </div>
+              ) : (
+                <div>
+                  <span className={styles.mouseCount}>{catsFood.present}</span>
+                  мышей в подарок
+                  <div>заказчик доволен</div>
+                </div>
+              )}
+            </span>
+          </div>
+          <img className={styles.img} src={catsFood.imgSrc} alt="img" />
+          <div className={styles.circle}>
+            <div className={styles.weight}>{catsFood.weight}</div>
+            <span className={styles.kg}>кг</span>
+          </div>
         </div>
       </div>
       {selected && !disabled ? (
@@ -85,7 +89,7 @@ const CatsFood: FC<ICatsFoodProps> = ({ className, catsFood, ...props }) => {
         <div className={styles.link}>
           <span className={styles.linkText}>Чего сидишь? Порадуй котэ,</span>
           <a href="#" onClick={() => setSelected(true)}>
-            Купи
+            купи
           </a>
         </div>
       )}
