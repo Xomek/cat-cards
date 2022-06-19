de65a55791f95019e4514d2b18195246bc479c4b
XomekX <z_v_m_x@mail.ru>
2022-06-19 15:10:25 +0700

    created ICatsFood interface and EWeightFood enum

---
+++ src/interfaces/catsFood.interface.ts
@@ -0,0 +1,14 @@
+export enum EWeightFood {
+  zeroFive = "0,5",
+  two = "2",
+  five = "5",
+}
+
+export interface ICatsFood {
+  id: string;
+  tagline: string;
+  title: string;
+  description: string;
+  weight: EWeightFood;
+  subText: string;
+}
